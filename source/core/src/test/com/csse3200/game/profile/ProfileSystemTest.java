faae4e2f509743f6f502a9b64f29fe9be893f832
pthneo <ben@schenk.com>
2025-08-24 09:49:59 +1000

    (#28) Write unit test for Profile System

---
+++ source/core/src/test/com/csse3200/game/profile/ProfileSystemTest.java
@@ -0,0 +1,32 @@
+package com.csse3200.game.profile;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.csse3200.game.extensions.GameExtension;
+
+@ExtendWith(GameExtension.class)
+class ProfileSystemTest {
+    @BeforeEach
+    void setUp() {
+        ProfileService.load();
+    }
+
+    @Test
+    void testLoadProfile() {
+        Profile profile = ProfileService.get();
+        assertNotNull(profile);
+    }
+
+    @Test
+    void testSaveProfile() {
+        ProfileService.get().setName("Testing Profile");
+        ProfileService.save();
+        ProfileService.load();
+        assertEquals("Testing Profile", ProfileService.get().getName());
+    }
+}
