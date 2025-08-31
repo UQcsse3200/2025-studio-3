692006a292329eebe39379801dca812195a812a6
Joseph Thorne <joseph.thorne@student.uq.edu.au>
2025-08-31 11:14:13 +1000

    removed extra call to playMusic() aas per comment in PR #62

--- source/core/src/main/com/csse3200/game/areas/LevelGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/LevelGameArea.java
@@ -54,14 +54,9 @@ public class LevelGameArea extends GameArea{
 
         float scale = 1.4f;
         spawnTiles(scale);
-        playMusic();
 
         playMusic();
-
     }
-
-
-
 
     private void loadAssets() {
         logger.debug("Loading assets");
