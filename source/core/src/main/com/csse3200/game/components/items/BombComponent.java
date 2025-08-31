3d6c06cdaabdb574fc9c178be93d0bf2bf4ed36c
Jack <133090850+jacklimmage@users.noreply.github.com>
2025-08-31 21:06:50 +1000

    items now take a reference parameter on use, to which they trigger an appropriate event

--- source/core/src/main/com/csse3200/game/components/items/BombComponent.java
+++ source/core/src/main/com/csse3200/game/components/items/BombComponent.java
@@ -1,22 +1,25 @@
 package com.csse3200.game.components.items;
 
-import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.math.Vector2;
 import com.csse3200.game.components.Component;
+import com.csse3200.game.entities.Entity;
 
 public class BombComponent extends Component {
 
     @Override
     public void create() {
-        // control how long the bomb takes to blow up
-        float fuseTime = 2f;
-        // creates a background task that triggers the event "boom"
-        Timer.schedule(new Timer.Task() {
-            @Override
-            public void run() {
-                entity.getEvents().trigger("boom");
-                entity.dispose(); // will need a blowing up animation later
-            }
-        }, fuseTime);
+        // assuming drag & drop, listen for dropped event (assumed successful placement)
+        entity.getEvents().addListener("dropped", this::onDropped);
+    }
+
+    private void onDropped(Entity[] enemies) {
+        // enemies contains all enemies in the range of the bombs explosion
+        for (Entity e : enemies) {
+            // iterate through enemies, triggering boom event
+            // (could be replaced with generic damage event
+            e.getEvents().trigger("boom");
+        }
+        entity.dispose();
     }
 
 }
--- source/core/src/main/com/csse3200/game/components/items/CoffeeComponent.java
+++ source/core/src/main/com/csse3200/game/components/items/CoffeeComponent.java
@@ -1,14 +1,23 @@
 package com.csse3200.game.components.items;
 
+
 import com.csse3200.game.components.Component;
+import com.csse3200.game.entities.Entity;
+
+import java.util.Vector;
 
 public class CoffeeComponent extends Component {
 
     @Override
     public void create() {
-        entity.getEvents().trigger("coffee");
-        // termination of the coffee effect must be controlled elsewhere
-        entity.dispose();
+        // assuming drag & drop, listen for dropped event (assumed successful placement)
+        entity.getEvents().addListener("dropped", this::onDropped);
     }
 
+    private void onDropped(Entity tower) {
+        // coffee entity has been dropped successfully on "tower"
+        tower.getEvents().trigger("coffee");
+        entity.dispose();
+        // item functionality logic handled in "tower"
+    }
 }
