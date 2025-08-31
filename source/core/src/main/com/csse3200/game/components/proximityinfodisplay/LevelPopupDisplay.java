c6f8f1a3485c3d8e8b82e4b78da0297f3357657e
Samuel Corby <s.corby@student.uq.edu.au>
2025-08-31 19:17:33 +1000

    Added comments LevelPopupDisplay. Fixed bug in LevelPopupDisplay -> update() where pressing Q did not close the window. Now pressing Q closes the window. Also changed update() from pressing E to Q for closing the popup display.

--- source/core/src/main/com/csse3200/game/components/proximityinfodisplay/LevelPopupDisplay.java
+++ source/core/src/main/com/csse3200/game/components/proximityinfodisplay/LevelPopupDisplay.java
@@ -11,10 +11,18 @@ import com.csse3200.game.entities.Entity;
 import com.csse3200.game.physics.BodyUserData;
 import com.csse3200.game.ui.UIComponent;
 
+/**
+ *  A class for implementing a popup display with level information.
+ */
 public class LevelPopupDisplay extends UIComponent {
+    /*popup window*/
     private Window popupDisplay;
+    /*popup window's visibility*/
     private boolean isDisplayed = false;
 
+    /**
+     *  Creation of the popup display and collision event.
+     */
     @Override
     public void create() {
         super.create();
@@ -24,40 +32,57 @@ public class LevelPopupDisplay extends UIComponent {
 
         // Creates popup display.
         popupDisplay = new Window ("Tree", skin);
-        //popupDisplay.setMovable(false);
+        popupDisplay.setMovable(false);
         popupDisplay.setSize(100, 200);
         popupDisplay.setPosition(
                 (Gdx.graphics.getWidth() - popupDisplay.getWidth()) / 2f,
                 (Gdx.graphics.getHeight() - popupDisplay.getHeight()) / 2f
         );
 
+        // Adds text in the popup display.
         Label message = new Label("Tree", skin);
         popupDisplay.add(message).pad(10).row();
 
+        // Sets popup display to false when created.
         popupDisplay.setVisible(false);
         stage.addActor(popupDisplay);
     }
 
+    /**
+     * Checks the status of the popup display
+     */
     @Override
     public void update() {
+
+        // Checks if popup display has been activated.
         if (!isDisplayed) {
             return;
         }
 
-        if (Gdx.input.isButtonJustPressed(Input.Keys.E)) {
+        // Press 'E' to close the popup display.
+        if (Gdx.input.isKeyJustPressed(Input.Keys.Q)) {
             popupDisplay.setVisible(false);
             isDisplayed = false;
         }
     }
 
-    private void onCollisionStart(Fixture me, Fixture other) {
+    /**
+     * Activates the popup display when the player collides with the entity.
+     * @param entity is the entity that the player collides with to activate the popup display.
+     * @param other is the player.
+     */
+    private void onCollisionStart(Fixture entity, Fixture other) {
         Entity otherEntity = ((BodyUserData) other.getBody().getUserData()).entity;
+        // Checks 'other' is the player by checking a component unique to the player
         if (otherEntity.getComponent(PlayerActions.class) != null) {
             popupDisplay.setVisible(true);
             isDisplayed = true;
         }
     }
 
+    /**
+     * Frees the memory.
+     */
     @Override
     public void dispose() {
         if (popupDisplay != null) {
@@ -67,6 +92,10 @@ public class LevelPopupDisplay extends UIComponent {
         super.dispose();
     }
 
+    /**
+     * Draws a sprite batch.
+     * @param batch Batch to render to.
+     */
     @Override
     protected void draw(SpriteBatch batch) {
 
