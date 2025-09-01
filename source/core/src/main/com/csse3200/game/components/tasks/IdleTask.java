c41cb1bdd431542dc72a7dd907d5b3563666f3ca
s-shalomi <sshalomi.p@gmail.com>
2025-09-01 23:10:31 +1000

    added multiple enemy handling where closest enemy is selected to attack

--- source/core/src/main/com/csse3200/game/components/tasks/IdleTask.java
+++ source/core/src/main/com/csse3200/game/components/tasks/IdleTask.java
@@ -10,19 +10,21 @@ import com.csse3200.game.physics.raycast.RaycastHit;
 import com.csse3200.game.rendering.DebugRenderer;
 import com.csse3200.game.services.ServiceLocator;
 
+import java.util.List;
+
 // stays idle until attack triggered
 public class IdleTask extends DefaultTask implements PriorityTask {
     private final float attackRange;
-    private final Entity target;
+    private final List<Entity> targets;
     private final PhysicsEngine physics;
     private final DebugRenderer debugRenderer;
     private final RaycastHit hit = new RaycastHit();
 
 
 
-    public IdleTask(float attackRange, Entity target) {
+    public IdleTask(float attackRange, List<Entity> targets) {
         this.attackRange = attackRange;
-        this.target = target;
+        this.targets = targets;
         physics = ServiceLocator.getPhysicsService().getPhysics();
         debugRenderer = ServiceLocator.getRenderService().getDebug();
     }
@@ -41,6 +43,10 @@ public class IdleTask extends DefaultTask implements PriorityTask {
     }
 
     private float getDistanceToTarget() {
+        Entity target = getNearestVisibleTarget();
+        if (target == null) {
+            return Float.MAX_VALUE;
+        }
         return owner.getEntity().getPosition().dst(target.getPosition());
     }
 
@@ -58,5 +64,36 @@ public class IdleTask extends DefaultTask implements PriorityTask {
             return 1;
         }
         return -1;
+    }
+
+    private boolean isTargetVisible(Entity target) {
+        Vector2 from = owner.getEntity().getCenterPosition();
+        Vector2 to = target.getCenterPosition();
+
+        // If there is an obstacle in the path to the player, not visible.
+        if (physics.raycast(from, to, PhysicsLayer.OBSTACLE, hit)) {
+            debugRenderer.drawLine(from, hit.point);
+            return false;
+        }
+        debugRenderer.drawLine(from, to);
+        return true;
+    }
+
+    private Entity getNearestVisibleTarget() {
+        Vector2 from = owner.getEntity().getCenterPosition();
+        Entity closestTarget = null;
+        float closestDist = Float.MAX_VALUE;
+
+        for (Entity target : targets) {
+            Vector2 targetPos = target.getCenterPosition();
+            float distance = from.dst(targetPos);
+            if (isTargetVisible(target) && distance <= attackRange) { // if target visible and in range
+                if (distance < closestDist) {
+                    closestDist = distance;
+                    closestTarget = target;
+                }
+            }
+        }
+        return closestTarget;
     }
 }
