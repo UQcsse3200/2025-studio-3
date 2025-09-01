f5fead839eb42447a538facfe1b7c87992e93528
zhafranrazz <zhafranrazz@gmail.com>
2025-09-02 06:46:25 +1000

    Add implemented EntitySpawn

--- source/core/src/main/com/csse3200/game/entities/EntitySpawn.java
+++ source/core/src/main/com/csse3200/game/entities/EntitySpawn.java
@@ -3,15 +3,53 @@ package com.csse3200.game.entities;
 import com.csse3200.game.entities.factories.WaveFactory;
 
 public class EntitySpawn {
-    WaveFactory waveFactory = new WaveFactory("wave1");
-    Entity[] entities = {};
-    public EntitySpawn(String wave) {
+    private final WaveFactory waveFactory;
 
+    // For now, only one robot type exists.
+    // Set this to the weight defined for that robot.
+    private final int robotWeight;
+    private Entity[] entities = new Entity[0];
+
+    public EntitySpawn(String wave) {
+        this(wave, /* robotWeight */ 2); // TODO: replace 2 with the actual robot weight
+    }
+
+    public EntitySpawn(String wave, int robotWeight) {
+        this.waveFactory = new WaveFactory(wave);
+        this.robotWeight = robotWeight;
+    }
+
+    public Entity[] getEntities() {
+        return entities;
     }
 
     public void spawnEnemies() {
-        int weight = waveFactory.getWaveWeight();
+        int waveWeight = waveFactory.getWaveWeight();
+        int minCount   = waveFactory.getMinZombiesSpawn();
 
+        if (robotWeight <= 0 || waveWeight <= 0) {
+            entities = new Entity[0];
+            return;
+        }
 
+        // If not divisible, add 1 toe the waveWeight
+        if (waveWeight % robotWeight != 0) {
+            waveWeight += 1;
+        }
+
+        // If the exact robotSpawn is still below the minimum, we can also bump robotSpawn up
+        int robotSpawn = waveWeight / robotWeight;
+        if (robotSpawn < minCount) {
+            robotSpawn = minCount;
+            waveWeight = robotSpawn * robotWeight;
+        }
+
+        // Builds the array of entities
+        Entity[] result = new Entity[robotSpawn];
+        for (int i = 0; i < robotSpawn; i++) {
+            // TODO: replace with the actual factory method from the robot team
+            result[i] = new Entity();
+        }
+        entities = result;
     }
 }
