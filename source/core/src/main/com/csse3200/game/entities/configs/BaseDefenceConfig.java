f1904c7e43df358ff6966a9d37a3b464b17a2f30
sam-patterson5 <samuelpatterson05@icloud.com>
2025-09-01 16:21:00 +1000

    Updates file contents in DefenceFactory & BaseDefenceConfig

---
+++ source/core/src/main/com/csse3200/game/entities/configs/BaseDefenceConfig.java
@@ -0,0 +1,10 @@
+package com.csse3200.game.entities.configs;
+
+
+/**
+ * Defines the properties stored in sigma config files to be loaded by the NPC Factory.
+ */
+public class BaseDefenceConfig extends BaseEntityConfig {
+    // define specific configurations here
+    // TODO Josh
+}
