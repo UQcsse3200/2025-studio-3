898f71c8cb45a5eaa254a55d955109cbb046f3e5
Louisa Wu <louisa.wu1@student.uq.edu.au>
2025-09-02 17:04:38 +1000

    added basic functionality to spawn enemies

--- source/core/src/main/com/csse3200/game/entities/EntitySpawn.java
+++ source/core/src/main/com/csse3200/game/entities/EntitySpawn.java
@@ -2,16 +2,39 @@ package com.csse3200.game.entities;
 
 import com.csse3200.game.entities.factories.WaveFactory;
 
-public class EntitySpawn {
-    WaveFactory waveFactory = new WaveFactory("wave1");
-    Entity[] entities = {};
-    public EntitySpawn(String wave) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
+public class EntitySpawn {
+    private final WaveFactory waveFactory;
+    private final Random random;
+
+    public EntitySpawn(String wave, Random random) {
+        this.waveFactory = new WaveFactory(wave);
+        this.random = random;
     }
 
-    public void spawnEnemies() {
-        int weight = waveFactory.getWaveWeight();
+    public List<Entity> spawnEnemies() {
+        int waveWeight = waveFactory.getWaveWeight();
+        Map<String, Integer> robotWeights = waveFactory.getEnemyWeights();
 
+        List<Entity> spawned = new ArrayList<>();
 
+        int currentWeight = 0;
+        List<String> robotTypes = new ArrayList<>(robotWeights.keySet());
+
+        while (currentWeight < waveWeight) {
+            String robotType = robotTypes.get(random.nextInt(robotTypes.size()));
+            int robotWeight = robotWeights.get(robotType);
+
+            if (currentWeight + robotWeight <= waveWeight) {
+                // Entity enemy = waveFactory.createRobot(robotType);
+                // spawned.add(enemy);
+                currentWeight += robotWeight;
+            }
+        }
+        return spawned;
     }
 }
