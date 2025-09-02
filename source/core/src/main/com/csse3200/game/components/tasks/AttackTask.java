252fa9ff27d423eec922c45c052527da9375fc87
s-shalomi <sshalomi.p@gmail.com>
2025-09-02 11:17:16 +1000

    abstracted attacking and idle behaviour

--- source/core/src/main/com/csse3200/game/components/tasks/AttackTask.java
+++ source/core/src/main/com/csse3200/game/components/tasks/AttackTask.java
@@ -18,12 +18,7 @@ import java.util.List;
  * Allows an entity to attack the closest target entity from a list of potential targets.
  * This task runs when there is a visible target within the entities range of attack
  */
-public class AttackTask extends DefaultTask implements PriorityTask {
-    private final List<Entity> targets;
-    private final float attackRange;
-    private final PhysicsEngine physics;
-    private final DebugRenderer debugRenderer;
-    private final RaycastHit hit = new RaycastHit();
+public class AttackTask extends TargetDetectionTasks {
     private MovementTask movementTask; // TODO: this should be projectiles
 
     /**
@@ -32,10 +27,7 @@ public class AttackTask extends DefaultTask implements PriorityTask {
      * @param attackRange the maximum distance the entity can find a target to attack
      */
     public AttackTask(List<Entity> targets, float attackRange) {
-        this.targets = targets;
-        this.attackRange = attackRange;
-        physics = ServiceLocator.getPhysicsService().getPhysics();
-        debugRenderer = ServiceLocator.getRenderService().getDebug();
+        super(targets, attackRange);
     }
 
     /**
@@ -89,45 +81,12 @@ public class AttackTask extends DefaultTask implements PriorityTask {
         movementTask.stop();
     }
 
-    // high priority only when close to enemy
-
-    /**
-     * Determines the tasks priority
-     * <ul>
-     *     <li>When active: returns {@code 1} if target is in range and visible, otherwise {@code -1}.</li>
-     *     <li>When inactive: returns {@code 1} if target is in range and visible, otherwise {@code -1}.</li>
-     * </ul>
-     * @return the tasks priority
-     */
-    @Override
-    public int getPriority() {
-        if (status == Status.ACTIVE) {
-            return getActivePriority();
-        }
-
-        return getInactivePriority();
-    }
-
-    /**
-     * Gets the distance to the nearest visible target
-     * @return the distance to the nearest target or a MAX VALUE if there is no target
-     */
-    private float getDistanceToTarget() {
-        Entity target = getNearestVisibleTarget();
-        if (target == null) {
-            return Float.MAX_VALUE;
-        }
-        return owner.getEntity().getPosition().dst(target.getPosition());
-    }
-
     /**
      * Determines the tasks priority when the task is running.
      * @return {@code 1} if a target is visible and within attack range,
      *         otherwise {@code -1}
      */
-    private int getActivePriority() {
-        float dst = getDistanceToTarget();
-        Entity target = getNearestVisibleTarget();
+    protected int getActivePriority(float dst, Entity target) {
         if (target == null) {
             return -1; // stop task if no target
         }
@@ -143,9 +102,7 @@ public class AttackTask extends DefaultTask implements PriorityTask {
      * @return {@code 1} if the target is visible and within attack range,
      *         otherwise {@code -1}
      */
-    private int getInactivePriority() {
-        float dst = getDistanceToTarget();
-        Entity target = getNearestVisibleTarget();
+    protected int getInactivePriority(float dst, Entity target) {
         if (target == null) {
             return -1;
         }
@@ -153,48 +110,5 @@ public class AttackTask extends DefaultTask implements PriorityTask {
             return 1; // start task if target is visible and in range
         }
         return -1;
-    }
-
-    /**
-     * Determines if a target is visible by checking for obstacles in the current entities line of sight
-     * @param target the target to check
-     * @return {@code true} if the target is visible, {@code false} otherwise
-     */
-    private boolean isTargetVisible(Entity target) {
-        Vector2 from = owner.getEntity().getCenterPosition();
-        Vector2 to = target.getCenterPosition();
-
-        // If there is an obstacle in the path to the player, not visible.
-        if (physics.raycast(from, to, PhysicsLayer.OBSTACLE, hit)) {
-            debugRenderer.drawLine(from, hit.point);
-            return false;
-        }
-        debugRenderer.drawLine(from, to);
-        return true;
-    }
-
-    /**
-     * Finds the nearest visible target within attack range.
-     *
-     * @return the closest visible target within range, or {@code null} if none
-     */
-    private Entity getNearestVisibleTarget() {
-        Vector2 from = owner.getEntity().getCenterPosition();
-        Entity closestTarget = null;
-        float closestDist = Float.MAX_VALUE;
-
-        for (Entity target : targets) {
-            Vector2 targetPos = target.getCenterPosition();
-            float distance = from.dst(targetPos);
-            if (isTargetVisible(target) && distance <= attackRange) { // if target visible and in range
-                if (distance < closestDist) {
-                    closestDist = distance;
-                    closestTarget = target;
-                }
-            }
-        }
-
-
-        return closestTarget;
     }
 }
