d37b639f827ab04e394680a077ca5b816e4c66cf
Matthew Moore <matthew.r.moore05@gmail.com>
2025-09-03 20:06:37 +1000

    Created a createBaseRobot function that constructs a base entity to construct more specific enemies.

--- source/core/src/main/com/csse3200/game/entities/factories/RobotFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/RobotFactory.java
@@ -18,6 +18,11 @@ import com.csse3200.game.services.ServiceLocator;
 
 public class RobotFactory {
     public static Entity createRobot(BaseEntityConfig config) {
+
+        // This creates pretty much everything except the animation
+        Entity robot = createBaseRobot(config);
+
+        //Animation
         final String atlasPath = "images/robot_placeholder.atlas";
         var rs = ServiceLocator.getResourceService();
 
@@ -28,25 +33,41 @@ public class RobotFactory {
         animator.addAnimation("angry", 0.1f, Animation.PlayMode.LOOP);
         animator.addAnimation("default", 1f, Animation.PlayMode.NORMAL);
 
+        robot.addComponent(animator);
+
+        animator.scaleEntity();
+        animator.startAnimation("chill"); // start an animation
+        // make a bit larger
+        robot.setScale(robot.getScale().x * 4f, robot.getScale().y * 4f);
+
+        return robot;
+    }
+
+    /**
+     * Initialises a Base Robot containing the features shared by all robots
+     * (e.g. combat stats, movement left, Physics, Hitbox)
+     * This robot can be used as a base entity by more specific robots.
+     *
+     * @param config A config file that contains the robot's stats.
+     * @return A robot entity. Note that it does not have an animator component.
+     */
+    private static Entity createBaseRobot(BaseEntityConfig config) {
+
         AITaskComponent aiComponent =
                 new AITaskComponent()
-                        .addTask(new MoveLeftTask(5f));
+                        .addTask(new MoveLeftTask(config.movementSpeed));
 
-        Entity robot = new Entity()
+        return new Entity()
                 .addComponent(new PhysicsComponent())
                 .addComponent(new PhysicsMovementComponent())
                 .addComponent(new ColliderComponent())
                 .addComponent(new HitboxComponent().setLayer(PhysicsLayer.NPC))
                 .addComponent(new CombatStatsComponent(config.health, config.baseAttack))
-                .addComponent(animator)
                 .addComponent(aiComponent);
 
-        animator.scaleEntity();
-        animator.startAnimation("chill"); // start an animation
-
-        // make a bit larger
-        robot.setScale(robot.getScale().x * 4f, robot.getScale().y * 4f);
-
-        return robot;
+        // The original NPCFactory had:
+        // PhysicsUtils.setScaledCollider(npc, 0.9f, 0.4f);
+        // and also .addComponent(new TouchAttackComponent(PhysicsLayer.PLAYER, 1.5f))
+        // I don't think we need that but I'm putting it here for reference
     }
 }
