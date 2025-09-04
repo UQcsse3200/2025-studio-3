62698288c0d0ab543adabfa7c06ccdb24060cd6c
Abhya Garg <abhyagarg2211@gmail.com>
2025-09-04 12:12:22 +1000

    Tested Interaction Handling

---
+++ source/core/src/test/com/csse3200/game/components/proximityandinfodisplay/LevelInteractionHandlingTest.java
@@ -0,0 +1,47 @@
+package com.csse3200.game.components.proximityandinfodisplay;
+
+import com.csse3200.game.components.proximityinfodisplay.LevelInteractionHandler;
+import com.csse3200.game.data.MenuSpriteData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LevelInteractionHandlingTest {
+
+    @Test
+    public void testLockedLevel() {
+        // Arrange
+        MenuSpriteData spriteData = new MenuSpriteData(
+                null, null, 0, 0,
+                "Locked Level",
+                "This level is locked",
+                "locked.png",
+                true // locked
+        );
+
+        // Act
+        LevelInteractionHandler.handleInteraction(spriteData);
+
+        // Assert → still locked
+        assertTrue(spriteData.getLocked());
+    }
+
+    @Test
+    public void testUnlockedLevel() {
+        // Arrange
+        MenuSpriteData spriteData = new MenuSpriteData(
+                null, null, 0, 0,
+                "Unlocked Level",
+                "This level is open",
+                "unlocked.png",
+                false // unlocked
+        );
+
+        // Act
+        LevelInteractionHandler.handleInteraction(spriteData);
+
+        // Assert → still unlocked
+        assertFalse(spriteData.getLocked());
+    }
+}
+
