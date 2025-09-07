ca2045ccd5e3ca0715fa63cc8bcf80659377b146
Harshil Walia <walia.harshil05@gmail.com>
2025-09-07 12:56:49 +1000

    Basic code for LaneRunner Minigame, added 2 files MinigameInputComponent.java, and PlayerLaneManager.java under minigames package

--- source/core/src/main/com/csse3200/minigame/PlayerLaneManager.java
+++ source/core/src/main/com/csse3200/minigame/PlayerLaneManager.java
@@ -1,24 +1,38 @@
 package com.csse3200.minigame;
 
+import com.csse3200.game.entities.Entity;
+
 public class PlayerLaneManager {
     private int currentLane;
     private int[] LaneIndices ={0,1,2};
+    private final LaneManager laneManager;
+    private final Entity player;
 
-    public PlayerLaneManager(int startingLane) {
-        if(startingLane < 0 || startingLane > 2){
-            throw new IllegalArgumentException("Invalid starting lane");
+    public PlayerLaneManager(Entity player, LaneManager laneManager) {
+        this.player = player;
+        this.laneManager = laneManager;
+        this.currentLane = 1;
+        movePlayertoLane(currentLane);
+    }
+    public void movePlayertoLane(int LaneIndex) {
+        if(LaneIndex<0||LaneIndex>=LaneIndices.length){
+            throw new IllegalArgumentException("Invalid lane index");
         }
-        this.currentLane = startingLane;
+        this.currentLane = LaneIndex;
+
+        float x = laneManager.getLaneCenter(LaneIndex);
+        float y= player.getPosition().y;
+        player.setPosition(x, y);
     }
     public int getCurrentLane() {
         return currentLane;
     }
-    public void setCurrentLane(int Lane) {
-        if(Lane < 0 || Lane > 2){
-            throw new IllegalArgumentException("Invalid lane");
-        }
-        this.currentLane = Lane;
-    }
+    //public void setCurrentLane(int Lane) {
+      //  if(Lane < 0 || Lane > 2){
+        //    throw new IllegalArgumentException("Invalid lane");
+        //}
+        //this.currentLane = Lane;
+    //}
     public int[] getLaneIndices() {
         return LaneIndices;
     }
