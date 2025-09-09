deff001592430096c7b3b49107133846d09107bb
cyric2020 <60243062+cyric2020@users.noreply.github.com>
2025-09-09 11:57:30 +1000

    (#119) Custom errors for cutscene validation

---
+++ source/core/src/main/com/csse3200/game/exceptions/ValidationError.java
@@ -0,0 +1,14 @@
+package com.csse3200.game.exceptions;
+
+/**
+ * Thrown when any {@link AuthoringError} is present after running a {@link com.csse3200.game.cutscene.validators.SchemaValidator}
+ */
+public class ValidationError extends RuntimeException {
+    public ValidationError() {
+        super();
+    }
+
+    public ValidationError(String message) {
+        super(message);
+    }
+}
