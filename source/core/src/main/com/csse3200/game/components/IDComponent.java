7a9940a7b0d8f126c5c65c34b026a5ad09c5af99
mannj5 <jcmann67@gmail.com>
2025-09-09 17:29:58 +1000

    Created an IDComponent for unique identification of an entity in the inventory

---
+++ source/core/src/main/com/csse3200/game/components/IDComponent.java
@@ -0,0 +1,13 @@
+package com.csse3200.game.components;
+
+public class IDComponent extends Component {
+    private String id;
+
+    public IDComponent(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+}
