a9e3953621fa218d2a4809c22e618223ba153691
s-shalomi <sshalomi.p@gmail.com>
2025-09-10 10:41:22 +1000

    created projectile component

--- source/core/src/main/com/csse3200/game/components/ProjectileComponent.java
+++ source/core/src/main/com/csse3200/game/components/ProjectileComponent.java
@@ -1,4 +1,13 @@
 package com.csse3200.game.components;
 
-public class ProjectileComponent {
+public class ProjectileComponent extends Component{
+    private final int damage;
+
+    public ProjectileComponent(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
 }
