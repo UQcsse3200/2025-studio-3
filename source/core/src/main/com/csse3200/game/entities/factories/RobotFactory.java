8ab04e76a969bb4c833bea96ecbd64c7e5813159
Sam_T <samt3658@gmail.com>
2025-09-11 22:02:57 +1000

    used formatter

--- source/core/src/main/com/csse3200/game/entities/factories/RobotFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/RobotFactory.java
@@ -12,7 +12,6 @@ import com.csse3200.game.components.tasks.RobotAttackTask;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.configs.*;
 import com.csse3200.game.physics.PhysicsLayer;
-import com.csse3200.game.physics.PhysicsUtils;
 import com.csse3200.game.physics.components.ColliderComponent;
 import com.csse3200.game.physics.components.HitboxComponent;
 import com.csse3200.game.physics.components.PhysicsComponent;
