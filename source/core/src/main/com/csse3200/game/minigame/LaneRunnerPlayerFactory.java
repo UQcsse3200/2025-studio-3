2b9cab294ee77819993b48cce245d60f5c605e54
Ansh Kataria <anshkataria@Anshs-MacBook-Air.local>
2025-09-13 14:38:13 +1000

    Modify LaneRunnerPlayerFactory.java

--- source/core/src/main/com/csse3200/game/minigame/LaneRunnerPlayerFactory.java
+++ source/core/src/main/com/csse3200/game/minigame/LaneRunnerPlayerFactory.java
@@ -1,29 +1,19 @@
 package com.csse3200.game.minigame;
 
 import com.csse3200.game.entities.Entity;
-import com.csse3200.game.physics.PhysicsLayer;
-import com.csse3200.game.physics.PhysicsUtils;
-import com.csse3200.game.physics.components.ColliderComponent;
-import com.csse3200.game.physics.components.HitboxComponent;
-import com.csse3200.game.physics.components.PhysicsComponent;
 import com.csse3200.game.rendering.TextureRenderComponent;
 
 public class LaneRunnerPlayerFactory {
     private static final String PLAYER_TEXTURE="images/box_boy.png";
 
-    public static Entity createPlayer(){
+    public static Entity createPlayer(LaneManager laneManager){
         Entity player = new Entity()
                 .addComponent(new TextureRenderComponent(PLAYER_TEXTURE))
-                //.addComponent(new PhysicsComponent())
-                //.addComponent(new ColliderComponent())
-                .addComponent(new HitboxComponent().setLayer(PhysicsLayer.PLAYER));
-
-       // PhysicsUtils.setScaledCollider(player,0.6f,0.3f);
-      //  player.getComponent(ColliderComponent.class).setDensity(1.5f);
-        player.getComponent(TextureRenderComponent.class).scaleEntity();
+                .addComponent(new PlayerMovementComponent(laneManager))
+                .addComponent(new MinigameInputComponent());
+        player.setScale(64f,64f);
+        int startingLane = 1;
+        player.setPosition(laneManager.getLaneCenter(startingLane),50f);
         return player;
-    }
-    public static String[] getTextures(){
-        return new String[]{PLAYER_TEXTURE};
     }
 }
