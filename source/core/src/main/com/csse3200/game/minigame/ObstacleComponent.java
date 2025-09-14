e5382f5dac2f65ff3367bcb52e38f3d45ce18aac
Ansh Kataria <anshkataria@Anshs-MacBook-Air.local>
2025-09-14 16:18:46 +1000

    Add Obstacle Component.

---
+++ source/core/src/main/com/csse3200/game/minigame/ObstacleComponent.java
@@ -0,0 +1,56 @@
+package com.csse3200.game.minigame;
+
+import com.csse3200.game.components.Component;
+
+public class ObstacleComponent extends Component {
+    private float speed;
+    private boolean isAlive;
+    private float screenBottom;
+
+
+    public ObstacleComponent(float speed) {
+        this.speed = speed;
+        this.isAlive = true;
+        this.screenBottom = 0.2f;
+
+    }
+
+    @Override
+    public void update() {
+        if (!isAlive) {
+            return;
+        }
+        moveDown();
+        if (isOffScreen()) {
+            entity.getEvents().trigger("dispose");
+        }
+    }
+    private void moveDown() {
+        float currentY = entity.getPosition().y;
+        float deltaTime = 1/60f; // Assuming 60 FPS, adjust as necessary
+        float newY = currentY - (speed * deltaTime);
+        entity.setPosition(entity.getPosition().x, newY);
+    }
+
+    private boolean isOffScreen() {
+        return entity.getPosition().y < screenBottom;
+    }
+    private void setSpeed(float speed) {
+        this.speed = speed;
+    }
+    private float getSpeed() {
+        return speed;
+    }
+    public void destroy() {
+        isAlive = false;
+    }
+    public boolean isAlive() {
+        return isAlive;
+    }
+    public void activate() {
+        isAlive = true;
+    }
+    public void setScreenBottom(float screenBottom) {
+        this.screenBottom = screenBottom;
+    }
+}
