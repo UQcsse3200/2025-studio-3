944af6f6602f1aaab8d827fed15503a2c352b5c9
s-shalomi <sshalomi.p@gmail.com>
2025-09-15 21:21:59 +1000

    updated touch attack of projectile

--- source/core/src/main/com/csse3200/game/entities/factories/ProjectileFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/ProjectileFactory.java
@@ -24,7 +24,7 @@ public class ProjectileFactory {
             .addComponent(new ColliderComponent())
             .addComponent(new HitboxComponent().setLayer(PhysicsLayer.PROJECTILE))
             .addComponent(new ProjectileComponent(damage))
-            .addComponent(new TouchAttackComponent(PhysicsLayer.ENEMY, 1.5f))
+            .addComponent(new TouchAttackComponent(PhysicsLayer.ENEMY, damage))
                 .addComponent(new CombatStatsComponent(1, damage)); // projectile should die on hit
 
     // Add render component so it draws above the grid
