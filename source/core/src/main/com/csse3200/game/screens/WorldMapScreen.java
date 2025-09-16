c2a59f2366915b48c315590397f70960701d7d4c
Simar Wadhawan <simarwadhawan2005@gmail.com>
2025-09-16 19:37:38 +1000

    added inventory and shop buttons on world map screen

--- source/core/src/main/com/csse3200/game/screens/WorldMapScreen.java
+++ source/core/src/main/com/csse3200/game/screens/WorldMapScreen.java
@@ -13,159 +13,224 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Json;
 import com.csse3200.game.GdxGame;
+import com.csse3200.game.persistence.Persistence;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Scene2D imports
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
 public class WorldMapScreen implements Screen {
-  private final GdxGame game;
-  private SpriteBatch batch;
-  private OrthographicCamera camera;
-  private static final Logger logger = LoggerFactory.getLogger(WorldMapScreen.class);
-  private Texture worldMap;
-  private Texture nodeCompleted, nodeUnlocked;
-  private Texture lockedLevel1, lockedLevel2;
-  private Texture playerTex, backButton;
+    private final GdxGame game;
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private static final Logger logger = LoggerFactory.getLogger(WorldMapScreen.class);
 
-  private Node[] nodes;
-  private Vector2 playerPos;
-  private float playerSpeed = 200f;
-  private Node nearbyNode = null;
+    private Texture worldMap;
+    private Texture nodeCompleted, nodeUnlocked;
+    private Texture lockedLevel1, lockedLevel2;
+    private Texture playerTex, backButton;
 
-  private Rectangle backBtnBounds;
-  private BitmapFont font;
+    private Node[] nodes;
+    private Vector2 playerPos;
+    private float playerSpeed = 200f;
+    private Node nearbyNode = null;
 
-  public WorldMapScreen(GdxGame game) {
-    this.game = game;
-  }
+    private Rectangle backBtnBounds;
+    private BitmapFont font;
 
-  @Override
-  public void show() {
-    batch = new SpriteBatch();
+    // NEW: Scene2D stage + UI
+    private Stage stage;
+    private Skin skin;
 
-    camera = new OrthographicCamera();
-    camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-    worldMap = new Texture(Gdx.files.internal("images/world_map.png"));
-    nodeCompleted = new Texture(Gdx.files.internal("images/node_completed.png"));
-    nodeUnlocked = new Texture(Gdx.files.internal("images/node_unlocked.png"));
-    lockedLevel1 = new Texture(Gdx.files.internal("images/locked_level1.png"));
-    lockedLevel2 = new Texture(Gdx.files.internal("images/locked_level2.png"));
-    playerTex = new Texture(Gdx.files.internal("images/character.png"));
-    backButton = new Texture(Gdx.files.internal("images/back_button.png"));
-
-    FileHandle file = Gdx.files.internal("data/nodes.json");
-    Json json = new Json();
-    nodes = json.fromJson(Node[].class, file);
-
-    playerPos =
-        new Vector2(nodes[0].px * Gdx.graphics.getWidth(), nodes[0].py * Gdx.graphics.getHeight());
-
-    backBtnBounds = new Rectangle(20, Gdx.graphics.getHeight() - 140, 120, 120);
-
-    font = new BitmapFont();
-    font.setColor(Color.WHITE);
-    font.getData().setScale(2f);
-  }
-
-  @Override
-  public void render(float delta) {
-    camera.update();
-    batch.setProjectionMatrix(camera.combined);
-
-    batch.begin();
-
-    batch.draw(worldMap, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-    batch.draw(
-        backButton, backBtnBounds.x, backBtnBounds.y, backBtnBounds.width, backBtnBounds.height);
-
-    for (Node node : nodes) {
-      Texture nodeTex;
-      if (node.completed) {
-        nodeTex = nodeCompleted;
-      } else if (node.unlocked) {
-        nodeTex = nodeUnlocked;
-      } else if (node.level == 2) {
-        nodeTex = lockedLevel1;
-      } else {
-        nodeTex = lockedLevel2;
-      }
-
-      float x = node.px * Gdx.graphics.getWidth();
-      float y = node.py * Gdx.graphics.getHeight();
-      batch.draw(nodeTex, x, y, 80, 80);
-
-      if (playerPos.dst(x, y) < 60) {
-        nearbyNode = node;
-        String prompt = (nearbyNode.level == 1) ? "Press E to Start" : "Press E to Checkpoint";
-        font.draw(batch, prompt, x, y + 100);
-      }
+    public WorldMapScreen(GdxGame game) {
+        this.game = game;
     }
 
-    batch.draw(playerTex, playerPos.x, playerPos.y, 96, 96);
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
 
-    batch.end();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-    handleInput(delta);
-  }
+        worldMap = new Texture(Gdx.files.internal("images/world_map.png"));
+        nodeCompleted = new Texture(Gdx.files.internal("images/node_completed.png"));
+        nodeUnlocked = new Texture(Gdx.files.internal("images/node_unlocked.png"));
+        lockedLevel1 = new Texture(Gdx.files.internal("images/locked_level1.png"));
+        lockedLevel2 = new Texture(Gdx.files.internal("images/locked_level2.png"));
+        playerTex = new Texture(Gdx.files.internal("images/character.png"));
+        backButton = new Texture(Gdx.files.internal("images/back_button.png"));
 
-  private void handleInput(float delta) {
-    float moveAmount = playerSpeed * delta;
+        FileHandle file = Gdx.files.internal("data/nodes.json");
+        Json json = new Json();
+        nodes = json.fromJson(Node[].class, file);
 
-    if (Gdx.input.isKeyPressed(Input.Keys.W)) playerPos.y += moveAmount;
-    if (Gdx.input.isKeyPressed(Input.Keys.S)) playerPos.y -= moveAmount;
-    if (Gdx.input.isKeyPressed(Input.Keys.A)) playerPos.x -= moveAmount;
-    if (Gdx.input.isKeyPressed(Input.Keys.D)) playerPos.x += moveAmount;
+        playerPos =
+                new Vector2(nodes[0].px * Gdx.graphics.getWidth(), nodes[0].py * Gdx.graphics.getHeight());
 
-    if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
-      float mx = Gdx.input.getX();
-      float my = Gdx.graphics.getHeight() - Gdx.input.getY();
+        backBtnBounds = new Rectangle(20, Gdx.graphics.getHeight() - 140, 120, 120);
 
-      if (backBtnBounds.contains(mx, my)) {
-        game.setScreen(GdxGame.ScreenType.MAIN_MENU);
-      }
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
+        font.getData().setScale(2f);
+
+        // === Scene2D UI ===
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        skin = new Skin(Gdx.files.internal("uiskin.json")); // default LibGDX skin
+
+        // Shop button
+        TextButton shopBtn = new TextButton("Shop", skin);
+        shopBtn.setPosition(Gdx.graphics.getWidth() - 220, 40);
+        shopBtn.setSize(100, 50);
+        shopBtn.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent event, Actor actor) {
+                        logger.info("🛒 Opening Shop");
+                        if (Persistence.profile() == null) {
+                            logger.info("Loading persistence before opening shop");
+                            Persistence.load();
+                        }
+                        game.setScreen(GdxGame.ScreenType.SHOP);
+                    }
+                });
+
+        // Inventory button
+        TextButton invBtn = new TextButton("Inventory", skin);
+        invBtn.setPosition(Gdx.graphics.getWidth() - 110, 40);
+        invBtn.setSize(100, 50);
+        invBtn.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent event, Actor actor) {
+                        logger.info("🎒 Opening Inventory");
+                        if (Persistence.profile() == null) {
+                            logger.info("Loading persistence before opening inventory");
+                            Persistence.load();
+                        }
+                        game.setScreen(GdxGame.ScreenType.INVENTORY);
+                    }
+                });
+
+        // Add buttons to stage
+        stage.addActor(shopBtn);
+        stage.addActor(invBtn);
     }
 
-    if (nearbyNode != null && Gdx.input.isKeyJustPressed(Input.Keys.E)) {
-      if (nearbyNode.level == 1) {
-        logger.info("🚀 Starting Level 1!");
-        game.setScreen(GdxGame.ScreenType.MAIN_GAME);
-      } else {
-        logger.info("✅ Checkpoint reached at Level {}", nearbyNode.level);
-        nearbyNode.unlocked = true;
-      }
+    @Override
+    public void render(float delta) {
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+
+        // Background map
+        batch.draw(worldMap, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        // Back button
+        batch.draw(backButton, backBtnBounds.x, backBtnBounds.y,
+                backBtnBounds.width, backBtnBounds.height);
+
+        // Draw level nodes
+        for (Node node : nodes) {
+            Texture nodeTex;
+            if (node.completed) {
+                nodeTex = nodeCompleted;
+            } else if (node.unlocked) {
+                nodeTex = nodeUnlocked;
+            } else if (node.level == 2) {
+                nodeTex = lockedLevel1;
+            } else {
+                nodeTex = lockedLevel2;
+            }
+
+            float x = node.px * Gdx.graphics.getWidth();
+            float y = node.py * Gdx.graphics.getHeight();
+            batch.draw(nodeTex, x, y, 80, 80);
+
+            if (playerPos.dst(x, y) < 60) {
+                nearbyNode = node;
+                String prompt = (nearbyNode.level == 1) ? "Press E to Start" : "Press E to Checkpoint";
+                font.draw(batch, prompt, x, y + 100);
+            }
+        }
+
+        // Player sprite
+        batch.draw(playerTex, playerPos.x, playerPos.y, 96, 96);
+
+        batch.end();
+
+        // Input handling
+        handleInput(delta);
+
+        // Draw Scene2D UI
+        stage.act(delta);
+        stage.draw();
     }
-  }
 
-  @Override
-  public void resize(int width, int height) {
-    camera.setToOrtho(false, width, height);
-  }
+    private void handleInput(float delta) {
+        float moveAmount = playerSpeed * delta;
 
-  @Override
-  public void pause() {
-    // Do nothing
-  }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) playerPos.y += moveAmount;
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) playerPos.y -= moveAmount;
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) playerPos.x -= moveAmount;
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) playerPos.x += moveAmount;
 
-  @Override
-  public void resume() {
-    // Do nothing
-  }
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+            float mx = Gdx.input.getX();
+            float my = Gdx.graphics.getHeight() - Gdx.input.getY();
 
-  @Override
-  public void hide() {
-    // Do nothing
-  }
+            if (backBtnBounds.contains(mx, my)) {
+                game.setScreen(GdxGame.ScreenType.MAIN_MENU);
+            }
+        }
 
-  @Override
-  public void dispose() {
-    batch.dispose();
-    worldMap.dispose();
-    nodeCompleted.dispose();
-    nodeUnlocked.dispose();
-    lockedLevel1.dispose();
-    lockedLevel2.dispose();
-    playerTex.dispose();
-    backButton.dispose();
-    font.dispose();
-  }
+        if (nearbyNode != null && Gdx.input.isKeyJustPressed(Input.Keys.E)) {
+            if (nearbyNode.level == 1) {
+                logger.info("🚀 Starting Level 1!");
+                game.setScreen(GdxGame.ScreenType.MAIN_GAME);
+            } else {
+                logger.info("✅ Checkpoint reached at Level {}", nearbyNode.level);
+                nearbyNode.unlocked = true;
+            }
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        camera.setToOrtho(false, width, height);
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {}
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        worldMap.dispose();
+        nodeCompleted.dispose();
+        nodeUnlocked.dispose();
+        lockedLevel1.dispose();
+        lockedLevel2.dispose();
+        playerTex.dispose();
+        backButton.dispose();
+        font.dispose();
+        stage.dispose();
+        skin.dispose();
+    }
 }
