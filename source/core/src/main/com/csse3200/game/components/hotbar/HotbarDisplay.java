a57bd17e758c8502ab96c596ed1d12e9f20729b5
nvassie <nathanvassie1@gmail.com>
2025-09-17 21:36:35 +1000

    made items one use in the hotbar

--- source/core/src/main/com/csse3200/game/components/hotbar/HotbarDisplay.java
+++ source/core/src/main/com/csse3200/game/components/hotbar/HotbarDisplay.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Array;
 import com.csse3200.game.areas.LevelGameArea;
 import com.csse3200.game.components.DeckInputComponent;
 import com.csse3200.game.entities.Entity;
@@ -23,11 +24,16 @@ public class HotbarDisplay extends UIComponent {
   private final float scaling;
   private final LevelGameArea game;
   private final Map<String, Supplier<Entity>> unitList;
+  private final Map<String, Supplier<Entity>> itemList;
+  private Array<Image> slotImages = new Array<>();
+  private float cellWidth;
+  private Group layered;
 
-  public HotbarDisplay(LevelGameArea game, Float scaling, Map<String, Supplier<Entity>> unitList) {
+  public HotbarDisplay(LevelGameArea game, Float scaling, Map<String, Supplier<Entity>> unitList, Map<String, Supplier<Entity>> itemList) {
     this.scaling = scaling;
     this.game = game;
     this.unitList = unitList;
+    this.itemList = itemList;
   }
 
   @Override
@@ -39,8 +45,8 @@ public class HotbarDisplay extends UIComponent {
   /**
    * This method creates the ui for the hotbar and the units that are selectable within its slots
    */
-  private void addActors() {
-    Group layered = new Group();
+private void addActors() {
+    layered = new Group();
 
     // create hotbar image
     Image hotbar = new Image(new Texture("images/hotbar.png"));
@@ -48,47 +54,71 @@ public class HotbarDisplay extends UIComponent {
 
     layered.setSize(hotbar.getPrefWidth(), hotbar.getPrefHeight());
 
-    // create selection star image and set it off screen
-    Image star = new Image(new Texture("images/selected_star.png"));
-    layered.addActor(star);
-    star.setPosition(-1000, -1000);
-    star.setSize(0.5f * scaling, 0.5f * scaling);
-    star.toFront();
-
     // initialise the values needed for placing unit images in slots
     float hotbarWidth = layered.getWidth();
-    float cellWidth = hotbarWidth / 6;
-    float x = cellWidth / 4;
+    cellWidth = hotbarWidth / 6;
+    float startX = cellWidth / 4;
     float y = 30;
+
     // creates unit images and places in slots
     for (Map.Entry<String, Supplier<Entity>> unit : unitList.entrySet()) {
-      Image tempUnit = new Image(new Texture(unit.getKey()));
-      tempUnit.setSize(scaling, scaling);
-      tempUnit.setPosition(x, y);
-      // creates listener that allows for selection and unselection of units
-      tempUnit.addListener(
-          new ClickListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-              if (event.getButton() == Input.Buttons.LEFT) {
-                game.setIsCharacterSelected(true);
-                game.beginDrag(new Texture(unit.getKey()));
-                Entity tempPlaceableUnit =
-                    new Entity()
-                        .addComponent(new DeckInputComponent(game, unit.getValue()))
-                        .addComponent(new TextureRenderComponent(unit.getKey()));
-                game.setSelectedUnit(tempPlaceableUnit);
-                star.setPosition(x + 1.5f * cellWidth / 4, 100);
-              } else if (event.getButton() == Input.Buttons.RIGHT) {
-                game.setSelectedUnit(null);
-                star.setPosition(-1000, -1000);
-              }
-              return false;
-            }
-          });
-      layered.addActor(tempUnit);
-      x += cellWidth;
+        Image tempUnit = new Image(new Texture(unit.getKey()));
+        tempUnit.setSize(scaling, scaling);
+
+        slotImages.add(tempUnit);
+
+        // listener for selection/use
+        tempUnit.addListener(
+            new ClickListener() {
+                @Override
+                public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                    if (event.getButton() == Input.Buttons.LEFT) {
+                        game.setIsCharacterSelected(true);
+                        game.beginDrag(new Texture(unit.getKey()));
+                        Entity tempPlaceableUnit =
+                            new Entity()
+                                .addComponent(new DeckInputComponent(game, unit.getValue()))
+                                .addComponent(new TextureRenderComponent(unit.getKey()));
+                        game.setSelectedUnit(tempPlaceableUnit);
+                    } else if (event.getButton() == Input.Buttons.RIGHT) {
+                        game.setSelectedUnit(null);
+                    }
+                    return false;
+                }
+            });
+        layered.addActor(tempUnit);
     }
+    for (Map.Entry<String, Supplier<Entity>> item : itemList.entrySet()) {
+        Image tempUnit = new Image(new Texture(item.getKey()));
+        tempUnit.setSize(scaling, scaling);
+
+        slotImages.add(tempUnit);
+
+        // listener for selection/use
+        tempUnit.addListener(
+            new ClickListener() {
+                @Override
+                public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                    if (event.getButton() == Input.Buttons.LEFT) {
+                        game.setIsCharacterSelected(true);
+                            game.beginDrag(new Texture(item.getKey()));
+                            Entity tempPlaceableUnit =
+                                new Entity()
+                                        .addComponent(new DeckInputComponent(game, item.getValue()))
+                                        .addComponent(new TextureRenderComponent(item.getKey()));
+                            game.setSelectedUnit(tempPlaceableUnit);
+                            remove(tempUnit);
+                    } else if (event.getButton() == Input.Buttons.RIGHT) {
+                        game.setSelectedUnit(null);
+                    }
+                    return false;
+                }
+            });
+
+        layered.addActor(tempUnit);
+    }
+
+    layoutUnits(startX, y, cellWidth);
 
     // sets the position to the top middle of screen
     hotbarTable = new Table();
@@ -107,7 +137,21 @@ public class HotbarDisplay extends UIComponent {
 
     stage.addActor(hotbarTable);
     hotbarTable.toBack();
-  }
+}
+
+public void remove(Image usedUnit) {
+    slotImages.removeValue(usedUnit, true);
+    usedUnit.remove();
+    layoutUnits(cellWidth / 4, 30, layered.getWidth() / 6);
+}
+
+private void layoutUnits(float startX, float y, float cellWidth) {
+    float x = startX;
+    for (Image img : slotImages) {
+        img.setPosition(x, y);
+        x += cellWidth;
+    }
+}
 
   @Override
   protected void draw(SpriteBatch batch) {
