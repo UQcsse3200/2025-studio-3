478e219d255894d4474dc5ed2a9a8aa8dfbf3e51
sam-patterson5 <samuelpatterson05@icloud.com>
2025-09-17 13:57:26 +1000

    Creates tests for the defender and generator stat components

--- source/core/src/main/com/csse3200/game/components/DefenderStatsComponent.java
+++ source/core/src/main/com/csse3200/game/components/DefenderStatsComponent.java
@@ -61,7 +61,11 @@ public class DefenderStatsComponent extends CombatStatsComponent {
 
   /** Sets the defender's attack range. */
   public void setRange(int range) {
-    this.range = range;
+    if (range < 0) {
+      this.range = 0;
+    } else {
+      this.range = range;
+    }
   }
 
   /**
@@ -85,7 +89,11 @@ public class DefenderStatsComponent extends CombatStatsComponent {
 
   /** Sets the defender's attack speed. */
   public void setAttackSpeed(int attackSpeed) {
-    this.attackSpeed = attackSpeed;
+    if (attackSpeed < 0) {
+      this.attackSpeed = 0;
+    } else {
+      this.attackSpeed = attackSpeed;
+    }
   }
 
   /**
