2333c336552c1625dd094408a6fdcfb54b20a23c
sam-patterson5 <samuelpatterson05@icloud.com>
2025-09-17 12:19:07 +1000

    Adds test files

---
+++ source/core/src/test/com/csse3200/game/entities/factories/DefenceFactoryTest.java
@@ -0,0 +1,21 @@
+package com.csse3200.game.entities.factories;
+
+import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.persistence.FileLoader;
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.csse3200.game.components.DefenderStatsComponent;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.configs.NPCConfigs;
+import com.csse3200.game.entities.factories.DefenceFactory;
+
+@ExtendWith(GameExtension.class)
+class DefenceFactoryTest {
+
+}
