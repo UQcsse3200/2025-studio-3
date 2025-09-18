4b11715bca0c87bc1b3271790c4269a336768a68
JS-0911 <janvhisharma2020@gmail.com>
2025-09-18 14:07:15 +1000

    fix(mainmenu): align MiniGames button with other menu buttons

--- source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
+++ source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
@@ -20,183 +20,176 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MainMenuDisplay extends UIComponent {
-  private static final Logger logger = LoggerFactory.getLogger(MainMenuDisplay.class);
-  private static final float Z_INDEX = 2f;
-  private Table table;
-  private Table miniGameBtnTable;
+    private static final Logger logger = LoggerFactory.getLogger(MainMenuDisplay.class);
+    private static final float Z_INDEX = 2f;
+    private Table table;
 
-  @Override
-  public void create() {
-    super.create();
+    @Override
+    public void create() {
+        super.create();
 
-    // Load assets
-    ServiceLocator.getResourceService().loadTextureAtlases(new String[] {"images/btn-blue.atlas"});
-    ServiceLocator.getResourceService().loadTextures(new String[] {"images/settings_icon.png"});
-    ServiceLocator.getResourceService().loadAll();
+        // Load assets
+        ServiceLocator.getResourceService().loadTextureAtlases(new String[] {"images/btn-blue.atlas"});
+        ServiceLocator.getResourceService().loadTextures(new String[] {"images/settings_icon.png"});
+        ServiceLocator.getResourceService().loadAll();
 
-    addActors();
-  }
+        addActors();
+    }
 
-  private TextButton makeTexturedButton(String text, TextureAtlas atlas, String region) {
-    Drawable background = new TextureRegionDrawable(atlas.findRegion(region));
+    private TextButton makeTexturedButton(String text, TextureAtlas atlas, String region) {
+        Drawable background = new TextureRegionDrawable(atlas.findRegion(region));
 
-    BitmapFont font = new BitmapFont();
-    font.getData().setScale(1.5f);
-    font.setColor(Color.BLACK);
+        BitmapFont font = new BitmapFont();
+        font.getData().setScale(1.5f);
+        font.setColor(Color.BLACK);
 
-    TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
-    style.up = background;
-    style.down = background;
-    style.over = background;
-    style.font = font;
+        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
+        style.up = background;
+        style.down = background;
+        style.over = background;
+        style.font = font;
 
-    TextButton button = new TextButton(text, style);
-    button.getLabel().setColor(new Color(0.1f, 0.1f, 0.1f, 1f));
-    return button;
-  }
+        TextButton button = new TextButton(text, style);
+        button.getLabel().setColor(new Color(0.1f, 0.1f, 0.1f, 1f));
+        return button;
+    }
 
-  private void addActors() {
-    table = new Table();
-    table.setFillParent(true);
+    private void addActors() {
+        table = new Table();
+        table.setFillParent(true);
 
-    // Title
-    Image title =
-        new Image(
-            ServiceLocator.getResourceService().getAsset("images/bg-text.png", Texture.class));
+        // Title
+        Image title =
+                new Image(
+                        ServiceLocator.getResourceService().getAsset("images/bg-text.png", Texture.class));
 
-    TextureAtlas buttonAtlas =
-        ServiceLocator.getResourceService().getAsset("images/btn-blue.atlas", TextureAtlas.class);
+        TextureAtlas buttonAtlas =
+                ServiceLocator.getResourceService().getAsset("images/btn-blue.atlas", TextureAtlas.class);
 
-    TextButton startBtn = makeTexturedButton("Start", buttonAtlas, "default");
-    TextButton quickStartBtn = makeTexturedButton("Quick Start", buttonAtlas, "default");
-    TextButton loadBtn = makeTexturedButton("Load", buttonAtlas, "default");
-    TextButton exitBtn = makeTexturedButton("Exit", buttonAtlas, "default");
-    TextButton miniGameBtn = makeTexturedButton("MiniGames", buttonAtlas, "default");
-    TextButton cutBtn = makeTexturedButton("Cutscene", buttonAtlas, "default");
+        TextButton startBtn = makeTexturedButton("Start", buttonAtlas, "default");
+        TextButton quickStartBtn = makeTexturedButton("Quick Start", buttonAtlas, "default");
+        TextButton loadBtn = makeTexturedButton("Load", buttonAtlas, "default");
+        TextButton miniGameBtn = makeTexturedButton("MiniGames", buttonAtlas, "default");
+        TextButton cutBtn = makeTexturedButton("Cutscene", buttonAtlas, "default");
+        TextButton exitBtn = makeTexturedButton("Exit", buttonAtlas, "default");
 
-    // --- Settings gear icon ---
-    Texture gearTexture =
-        ServiceLocator.getResourceService().getAsset("images/settings_icon.png", Texture.class);
-    ImageButton.ImageButtonStyle gearStyle = new ImageButton.ImageButtonStyle();
-    gearStyle.imageUp = new TextureRegionDrawable(gearTexture);
+        // --- Settings gear icon ---
+        Texture gearTexture =
+                ServiceLocator.getResourceService().getAsset("images/settings_icon.png", Texture.class);
+        ImageButton.ImageButtonStyle gearStyle = new ImageButton.ImageButtonStyle();
+        gearStyle.imageUp = new TextureRegionDrawable(gearTexture);
 
-    ImageButton settingsIcon = new ImageButton(gearStyle);
-    settingsIcon.setSize(90f, 90f);
-    settingsIcon.setPosition(Gdx.graphics.getWidth() - 100f, Gdx.graphics.getHeight() - 120f);
+        ImageButton settingsIcon = new ImageButton(gearStyle);
+        settingsIcon.setSize(90f, 90f);
+        settingsIcon.setPosition(Gdx.graphics.getWidth() - 100f, Gdx.graphics.getHeight() - 120f);
 
-    settingsIcon.addListener(
-        new ChangeListener() {
-          @Override
-          public void changed(ChangeEvent changeEvent, Actor actor) {
-            logger.debug("Settings icon clicked");
-            entity.getEvents().trigger("settings");
-          }
-        });
+        settingsIcon.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("Settings icon clicked");
+                        entity.getEvents().trigger("settings");
+                    }
+                });
 
-    // Button listeners
-    startBtn.addListener(
-        new ChangeListener() {
-          @Override
-          public void changed(ChangeEvent changeEvent, Actor actor) {
-            logger.debug("Start button clicked");
-            entity.getEvents().trigger("worldMap");
-          }
-        });
+        // Button listeners
+        startBtn.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("Start button clicked");
+                        entity.getEvents().trigger("worldMap");
+                    }
+                });
 
-    quickStartBtn.addListener(
-        new ChangeListener() {
-          @Override
-          public void changed(ChangeEvent changeEvent, Actor actor) {
-            logger.debug("Quick Start button clicked");
-            entity.getEvents().trigger("quickStart");
-          }
-        });
+        quickStartBtn.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("Quick Start button clicked");
+                        entity.getEvents().trigger("quickStart");
+                    }
+                });
 
-    loadBtn.addListener(
-        new ChangeListener() {
-          @Override
-          public void changed(ChangeEvent changeEvent, Actor actor) {
-            logger.debug("Load button clicked");
-            entity.getEvents().trigger("load");
-          }
-        });
+        loadBtn.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("Load button clicked");
+                        entity.getEvents().trigger("load");
+                    }
+                });
 
-    miniGameBtn.addListener(
-        new ChangeListener() {
-          @Override
-          public void changed(ChangeEvent changeEvent, Actor actor) {
-            logger.debug("MiniGames button clicked");
-            entity.getEvents().trigger("minigame");
-          }
-        });
+        miniGameBtn.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("MiniGames button clicked");
+                        entity.getEvents().trigger("minigame");
+                    }
+                });
 
-    cutBtn.addListener(
-        new ChangeListener() {
-          @Override
-          public void changed(ChangeEvent changeEvent, Actor actor) {
-            logger.debug("Cutscene button clicked");
-            entity.getEvents().trigger("Cutscene");
-          }
-        });
+        cutBtn.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("Cutscene button clicked");
+                        entity.getEvents().trigger("Cutscene");
+                    }
+                });
 
-    exitBtn.addListener(
-        new ChangeListener() {
-          @Override
-          public void changed(ChangeEvent changeEvent, Actor actor) {
-            logger.debug("Exit button clicked");
-            entity.getEvents().trigger("exit");
-          }
-        });
+        exitBtn.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("Exit button clicked");
+                        entity.getEvents().trigger("exit");
+                    }
+                });
 
-    // Layout
-    table.center();
-    float xf = 0.40f;
-    table
-        .add(title)
-        .size(title.getWidth() * xf, title.getHeight() * xf)
-        .top()
-        .center()
-        .padTop(20f)
-        .padBottom(20f);
-    table.row();
+        // Layout
+        table.center();
+        float xf = 0.40f;
+        table
+                .add(title)
+                .size(title.getWidth() * xf, title.getHeight() * xf)
+                .top()
+                .center()
+                .padTop(20f)
+                .padBottom(20f);
+        table.row();
 
-    float buttonWidth = 200f;
-    float buttonHeight = 50f;
+        float buttonWidth = 200f;
+        float buttonHeight = 50f;
 
-    table.add(startBtn).size(buttonWidth, buttonHeight).padBottom(5f);
-    table.row();
-    table.add(quickStartBtn).size(buttonWidth, buttonHeight).padBottom(5f);
-    table.row();
-    table.add(loadBtn).size(buttonWidth, buttonHeight).padBottom(5f);
-    table.row();
-    table.add(exitBtn).size(buttonWidth, buttonHeight).padBottom(5f);
-    table.row();
-    table.add(cutBtn).size(buttonWidth, buttonHeight).padBottom(5f);
+        table.add(startBtn).size(buttonWidth, buttonHeight).padBottom(5f);
+        table.row();
+        table.add(quickStartBtn).size(buttonWidth, buttonHeight).padBottom(5f);
+        table.row();
+        table.add(loadBtn).size(buttonWidth, buttonHeight).padBottom(5f);
+        table.row();
+        table.add(miniGameBtn).size(buttonWidth, buttonHeight).padBottom(5f); // moved into main flow
+        table.row();
+        table.add(cutBtn).size(buttonWidth, buttonHeight).padBottom(5f);
+        table.row();
+        table.add(exitBtn).size(buttonWidth, buttonHeight).padBottom(5f);
 
-    // MiniGame button bottom-left
-    miniGameBtnTable = new Table();
-    miniGameBtnTable.bottom().left();
-    miniGameBtnTable.padBottom(20f).padLeft(20f);
-    miniGameBtnTable.add(miniGameBtn);
+        // Add actors
+        stage.addActor(table);
+        stage.addActor(settingsIcon);
+    }
 
-    // Add actors
-    stage.addActor(table);
-    stage.addActor(settingsIcon);
-    stage.addActor(miniGameBtnTable);
-  }
+    @Override
+    public void draw(SpriteBatch batch) {}
 
-  @Override
-  public void draw(SpriteBatch batch) {}
+    @Override
+    public float getZIndex() {
+        return Z_INDEX;
+    }
 
-  @Override
-  public float getZIndex() {
-    return Z_INDEX;
-  }
-
-  @Override
-  public void dispose() {
-    table.clear();
-    miniGameBtnTable.clear();
-    super.dispose();
-  }
+    @Override
+    public void dispose() {
+        table.clear();
+        super.dispose();
+    }
 }
