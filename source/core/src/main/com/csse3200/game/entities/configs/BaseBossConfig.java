06aa1a6af67f1ffdc4e0bfed238c829320e90c02
Ansh Kataria <ansh.kataria@student.uq.edu.au>
2025-09-23 00:02:19 +1000

    WIP: Boss Factory and Boss Config.

---
+++ source/core/src/main/com/csse3200/game/entities/configs/BaseBossConfig.java
@@ -0,0 +1,9 @@
+package com.csse3200.game.entities.configs;
+
+public class BaseBossConfig {
+    public String name = "Boss";
+    public String atlasFilePath = "images/boss/boss.atlas";
+    public int health = 500;
+    public int attack = 50;
+    public float scale = 4.0f;
+}
