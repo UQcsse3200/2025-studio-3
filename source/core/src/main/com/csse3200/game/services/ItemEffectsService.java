657027b321f2753bb61089c5966882eb045b9cbe
BiancaRose2 <bianca.leathers@gmail.com>
2025-10-01 23:49:57 +1000

    Added volume specification to sound #230

--- source/core/src/main/com/csse3200/game/services/ItemEffectsService.java
+++ source/core/src/main/com/csse3200/game/services/ItemEffectsService.java
@@ -76,10 +76,9 @@ public class ItemEffectsService {
     } catch (Exception ignored) {
       // ResourceService may throw if asset not loaded
     }
-    if (effectSound != null) {
-      effectSound.play();
-      logger.info("Sound played {}", animatorName);
-    }
+    float volume = ServiceLocator.getSettingsService().getSoundVolume();
+    effectSound.play(volume);
+    logger.info("Sound played {}", animatorName);
 
     // Attach component to handle optional movement and timed disposal
     effect.addComponent(
