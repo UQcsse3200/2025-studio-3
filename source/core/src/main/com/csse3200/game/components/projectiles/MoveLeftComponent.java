88871bd5333017eccb6ffc44439d92433b56be13
A-shuk <a77shukla@gmail.com>
2025-10-02 16:03:54 +1000

    added moveleftcomponent

---
+++ source/core/src/main/com/csse3200/game/components/projectiles/MoveLeftComponent.java
@@ -0,0 +1,34 @@
+package com.csse3200.game.components.projectiles;
+
+
+import com.badlogic.gdx.math.Vector2;
+import com.csse3200.game.components.Component;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.services.ServiceLocator;
+
+public class MoveLeftComponent extends Component {
+    private float speed;
+    private boolean shoot = false;
+
+    public MoveLeftComponent(float speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public void update() {
+        float delta = ServiceLocator.getTimeSource().getDeltaTime();
+        Entity entity = getEntity(); // from the Component
+
+        Vector2 pos = entity.getPosition();
+        entity.setPosition(pos.x - speed * delta, pos.y); //move left
+
+        if (shoot) {
+            entity.getEvents().trigger("spawnProjectile", entity.getPosition());
+            shoot = false;
+        }
+    }
+
+    public void setShoot(boolean shoot) {
+        this.shoot = shoot;
+    }
+}
