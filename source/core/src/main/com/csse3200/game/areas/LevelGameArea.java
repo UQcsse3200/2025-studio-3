a65f8661ce1d8d3f972bfc2c9b8aa44f530e44ae
sam-patterson5 <samuelpatterson05@icloud.com>
2025-10-07 23:51:05 +1000

    Adds new files for shield animation

--- source/core/src/main/com/csse3200/game/areas/LevelGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/LevelGameArea.java
@@ -568,12 +568,10 @@ public class LevelGameArea extends GameArea implements AreaAPI, EnemySpawner {
         .addListener(
             "fire",
             (TargetDetectionTasks.AttackDirection direction) -> {
-              if (newEntity.getComponent(ProjectileComponent.class) != null) {
-                spawnProjectile(
-                  entityPos,
-                  newEntity.getComponent(ProjectileComponent.class).getProjectile(),
-                  direction);
-              } 
+              spawnProjectile(
+                entityPos,
+                newEntity.getComponent(ProjectileComponent.class).getProjectile(),
+                direction);
               newEntity.getEvents().trigger("attackStart");
             });
     setIsCharacterSelected(false);
