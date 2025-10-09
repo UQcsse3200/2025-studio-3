93bcac82cb0443f16cf964a6ab5f9b4eb6722bd0
Abhya Garg <abhyagarg2211@gmail.com>
2025-10-10 03:02:39 +1000

    Fix ChoiceAction button creation and missing WorldMapScreen methods

---
+++ source/core/src/main/com/csse3200/game/ui/ButtonFactory.java
@@ -0,0 +1,25 @@
+package com.csse3200.game.ui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+/**
+ * Utility class for creating buttons with a consistent style.
+ */
+public class ButtonFactory {
+
+    // Load the skin once (replace the path if your skin is stored elsewhere)
+    private static final Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+
+    /**
+     * Creates a text button with the given label.
+     *
+     * @param text The text to display on the button.
+     * @return A new Button instance.
+     */
+    public static Button createButton(String text) {
+        return new TextButton(text, skin);
+    }
+}
