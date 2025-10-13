e23b6999da9b5e1165b8a305a0169f1387c81326
Kostya Reznik <k.reznik@student.uq.edu.au>
2025-10-13 14:29:06 +1000

    Buffed defences (cheaper and higher damage) and increased initial scrap to make it so you can at least beat the first wave

--- source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
+++ source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
@@ -204,7 +204,7 @@ public class MainGameScreen extends ScreenAdapter {
     ServiceLocator.registerResourceService(new ResourceService());
     ServiceLocator.registerEntityService(new EntityService());
     ServiceLocator.registerRenderService(new RenderService());
-    ServiceLocator.registerCurrencyService(new CurrencyService(50, 10000));
+    ServiceLocator.registerCurrencyService(new CurrencyService(100, 10000));
     ServiceLocator.registerItemEffectsService(new ItemEffectsService());
     ServiceLocator.registerWaveService(new WaveService());
     renderer = RenderFactory.createRenderer();
