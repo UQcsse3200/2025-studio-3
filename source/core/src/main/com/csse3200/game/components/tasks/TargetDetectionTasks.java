d2b9bf94b97e4b600bd503fce9d07bf466ca345c
shalomi <sshalomi.p@gmail.com>
2025-10-17 14:51:47 +1000

    spotlessApply

--- source/core/src/main/com/csse3200/game/components/tasks/TargetDetectionTasks.java
+++ source/core/src/main/com/csse3200/game/components/tasks/TargetDetectionTasks.java
@@ -112,7 +112,7 @@ public abstract class TargetDetectionTasks extends DefaultTask implements Priori
     LevelGameArea area = (LevelGameArea) ServiceLocator.getGameArea();
     float tileSize = area.getTileSize();
 
-      // done with the help of OpenAI
+    // done with the help of OpenAI
     for (float yOffset = -tileSize; yOffset <= tileSize; yOffset += 0.5) {
       offsetFrom.set(from.x, from.y + yOffset);
       end.set(offsetFrom).mulAdd(castDir, attackRange);
