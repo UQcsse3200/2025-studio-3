0677c9cff5eae1726a158d48fc2fa8c14fddfc9b
Jack <133090850+jacklimmage@users.noreply.github.com>
2025-10-22 01:11:19 +1000

    formatting and defensive programming to pass tests

--- source/core/src/main/com/csse3200/game/areas/LevelGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/LevelGameArea.java
@@ -1231,11 +1231,16 @@ public class LevelGameArea extends GameArea implements AreaAPI, EnemySpawner {
       if (levelCompleteEntity != null) {
         levelCompleteEntity.getEvents().trigger("levelComplete");
         // play win sound
-        Sound sound =
-            ServiceLocator.getResourceService().getAsset("sounds/level-win.mp3", Sound.class);
-        float volume = ServiceLocator.getSettingsService().getSoundVolume();
-        sound.play(volume);
-        logger.info("Played sound: sounds/level-win.mp3");
+        if (ServiceLocator.getResourceService() != null
+            && ServiceLocator.getSettingsService() != null) {
+          Sound sound =
+              ServiceLocator.getResourceService().getAsset("sounds/level-win.mp3", Sound.class);
+          if (sound != null) {
+            float volume = ServiceLocator.getSettingsService().getSoundVolume();
+            sound.play(volume);
+            logger.info("Played sound: sounds/level-win.mp3");
+          }
+        }
       }
 
       GameStateService service = ServiceLocator.getGameStateService();
