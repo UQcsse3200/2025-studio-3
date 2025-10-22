d366236460aff9d21cd9ce5ae6beb8eaa64ec1b7
Kostya Reznik <k.reznik@student.uq.edu.au>
2025-10-22 21:49:16 +1000

    One character fix

--- source/core/src/main/com/csse3200/game/areas/LevelGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/LevelGameArea.java
@@ -1266,7 +1266,7 @@ public class LevelGameArea extends GameArea implements AreaAPI, EnemySpawner {
     }
 
     int currentWave = ServiceLocator.getWaveService().getCurrentWave();
-    if (currentWave >= ServiceLocator.getWaveService().getCurrentLevelWaveCount()) {
+    if (currentWave > ServiceLocator.getWaveService().getCurrentLevelWaveCount()) {
       logger.info("Level is complete!");
       isLevelComplete = true;
       if (levelCompleteEntity != null) {
