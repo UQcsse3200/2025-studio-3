57d68e61a20e44cc5480c0cc46661c5afcb4877f
Cate <cbrown04562@gmail.com>
2025-10-22 15:33:43 +1000

    Removes multiple dossier button bug

--- source/core/src/main/com/csse3200/game/progression/arsenal/Arsenal.java
+++ source/core/src/main/com/csse3200/game/progression/arsenal/Arsenal.java
@@ -127,13 +127,13 @@ public class Arsenal {
 
     for (Map.Entry<String, BaseDefenderConfig> entry : getAllDefences().entrySet()) {
       if (entry.getValue().getLevelUnlockedOn().equals(INITIAL_DEFENCE)
-          && !entry.getKey().equals("wall")) {
+          && !entry.getKey().equals("wall") && !defences.contains(entry.getKey())) {
         defences.add(entry.getKey());
       }
     }
 
     for (Map.Entry<String, BaseGeneratorConfig> entry : getAllGenerators().entrySet()) {
-      if (entry.getValue().getLevelUnlockedOn().equals(INITIAL_DEFENCE)) {
+      if (entry.getValue().getLevelUnlockedOn().equals(INITIAL_DEFENCE) && !generators.contains(entry.getKey())) {
         generators.add(entry.getKey());
       }
     }
